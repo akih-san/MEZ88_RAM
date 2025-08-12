e02299a5f857045b0fcf066b892c8222112fe771
@Akih_san <112370246+akih-san@users.noreply.github.com>
2025-08-12 12:54:09 +0900

    Add files via upload

---
+++ MEZ88.X/build/default/production/_ext/1360937237/I2C_IO.p1.d
@@ -0,0 +1,6 @@
+build/default/production/_ext/1360937237/I2C_IO.p1:  \
+../src/I2C_IO.c  \
+../src/../src/mez88_47q.h  \
+../src/../src/../src/picconfig.h  \
+../src/../src/../fatfs/ff.h  \
+../src/../src/../fatfs/../fatfs/ffconf.h 
